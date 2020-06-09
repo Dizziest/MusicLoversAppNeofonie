909b107d22ae4a1483f9b592de98c16313b5ba51
Dizziest <marcin.michalowski45@gmail.com>
2020-06-09 16:19:31 +0200

    Added data class for AlbumsResponse.

---
+++ app/src/main/java/com/example/musicloversappneofonie/responses/AlbumsResponse.kt
@@ -0,0 +1,9 @@
+package com.example.musicloversappneofonie.responses
+
+import com.example.musicloversappneofonie.models.Album
+import com.google.gson.annotations.SerializedName
+
+data class AlbumsResponse(
+    @SerializedName("results")
+    val results: List<Album>
+)
